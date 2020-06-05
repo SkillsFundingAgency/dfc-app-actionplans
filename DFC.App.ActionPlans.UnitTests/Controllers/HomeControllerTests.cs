8d540e27a177a2674bdffedf5278c39147111861
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-06-05 16:02:24 +0100

    wip

---
+++ DFC.App.ActionPlans.UnitTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Dfc.App.ActionPlans.Controllers;
+using DFC.App.ActionPlans.Models;
+using DFC.App.ActionPlans.Services.DSS.Interfaces;
+using DFC.App.ActionPlans.Services.DSS.Models;
+using DFC.App.ActionPlans.ViewModels;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace DFC.App.ActionPlans.UnitTests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private IOptions<CompositeSettings> _compositeSettings;
+        private ILogger<HomeController> _logger;
+        private IOptions<AuthSettings> _authSettings;
+        private IDssReader _dssReader;
+        private HomeController controller;
+
+        [SetUp]
+        public void Init()
+        {
+            _logger = new Logger<HomeController>(new LoggerFactory());
+            _compositeSettings = Options.Create(new CompositeSettings());
+            _logger = Substitute.For<ILogger<HomeController>>();
+            _authSettings = Options.Create(new AuthSettings
+            {
+                RegisterUrl = "reg", SignInUrl = "signin", SignOutUrl = "signout"
+            });
+            controller = new HomeController(_logger, _compositeSettings,_authSettings, _dssReader);
+            controller.ControllerContext.HttpContext = new DefaultHttpContext();
+        }
+
+        [Test]
+        public void WhenHeadCalled_ReturnHtml()
+        {
+            var result = controller.Head() as ViewResult;
+            var vm = new HeadViewModel {PageTitle = "Page Title",};
+            var pageTitle = vm.PageTitle;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task WhenBodyCalled_ReturnHtml()
+        {
+            var result = await controller.Body() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public void WhenBreadCrumbCalled_ReturnHtml()
+        {
+            var result = controller.Breadcrumb() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task WhenBodyTopCalled_ReturnHtml()
+        {
+            var result = await controller.BodyTop() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task WhenBodyTopCalled_ReturnHtmlWithUserName()
+        {
+            var customer = new Customer()
+            {
+                CustomerId = new Guid("c2e27821-cc60-4d3d-b4f0-cbe20867897c"),
+                FamilyName = "familyName",
+                GivenName = "givenName"
+            };
+        
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    HttpContext =
+                    {
+                        User = new ClaimsPrincipal(
+                            new ClaimsIdentity(new List<Claim> {new Claim("CustomerId", "test")},
+                                "testType"))
+                    }
+                }
+            };
+            var result = await controller.BodyTop() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+            var model = result.ViewData.Model as CompositeViewModel;
+            model.Name.Should().Be("givenName familyName");
+        }
+
+        [Test]
+        public void WhenBodyFooterCalled_ReturnHtml()
+        {
+            var result = controller.BodyFooter() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public void WhenErrorCalled_ReturnHtml()
+        {
+            var result = controller.Error() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+
+       
+    }
+}
+
+
---
+++ DFC.App.ActionPlans/ViewModels/HeadViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFC.App.ActionPlans.ViewModels
+{
+    public class HeadViewModel
+    {
+        private const string DefaultPageTitle = "Action Plans";
+
+        public string PageTitle { get; set; } = DefaultPageTitle;
+    }
+}
