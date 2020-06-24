acefa57be864b371c560459cdb85794ebe6e4b09
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-06-24 13:48:07 +0100

    wip

--- DFC.App.ActionPlans.UnitTests/Controllers/ViewActionControllerTests.cs
+++ DFC.App.ActionPlans.UnitTests/Controllers/ViewActionControllerTests.cs
@@ -24,7 +24,15 @@ namespace DFC.App.ActionPlans.UnitTests.Controllers
         }
 
        
-        
+
+        [Test]
+        public async Task WhenBodyCalled_ReturnHtml()
+        {
+            var result = await _controller.Body() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
         [Test]
         public async Task WhenBodyCalledWithParameters_ReturnHtml()
         {
@@ -34,9 +42,57 @@ namespace DFC.App.ActionPlans.UnitTests.Controllers
             result.ViewName.Should().BeNull();
         }
 
-       
+        [Test]
+        public void WhenBreadCrumbCalled_ReturnHtml()
+        {
+            var result = _controller.Breadcrumb() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
 
-       
+        [Test]
+        public async Task WhenBodyTopCalled_ReturnHtml()
+        {
+            var result = await _controller.BodyTop() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task WhenBodyTopCalled_ReturnHtmlWithUserName()
+        {
+          
+        
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    HttpContext =
+                    {
+                        User = new ClaimsPrincipal(
+                            new ClaimsIdentity(new List<Claim> {new Claim("CustomerId", "test")},
+                                "testType"))
+                    }
+                }
+            };
+            var result = await _controller.BodyTop() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+            var model = result.ViewData.Model as CompositeViewModel;
+            model.Name.Should().NotBe(null);
+        }
+
+        [Test]
+        public void WhenBodyFooterCalled_ReturnHtml()
+        {
+            var result = _controller.BodyFooter() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
 
    
     }
