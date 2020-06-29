6809c2406866af0e13022e05b3ffee41c34a9bc3
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-06-29 09:41:13 +0100

    Added Tests

---
+++ DFC.App.ActionPlans.UnitTests/Controllers/ChangeGoalDueDateControllerTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Dfc.App.ActionPlans.Controllers;
+using DFC.App.ActionPlans.Controllers;
+using DFC.App.ActionPlans.Services.DSS.Models;
+using DFC.App.ActionPlans.ViewModels;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using NUnit.Framework;
+
+namespace DFC.App.ActionPlans.UnitTests.Controllers
+{
+    class ChangeGoalDueDateControllerTests : BaseControllerTests
+    {
+         private ChangeGoalDueDateController _controller;
+
+        [SetUp]
+        public void Init()
+        {
+           
+            _controller = new ChangeGoalDueDateController(_logger, _compositeSettings, _dssReader,_dssWriter);
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+           
+        }
+
+        [Test]
+        public void WhenHeadCalled_ReturnHtml()
+        {
+            var result = _controller.Head() as ViewResult;
+            var vm = new HeadViewModel {PageTitle = "Page Title",};
+            var pageTitle = vm.PageTitle;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task WhenBodyCalled_ReturnHtml()
+        {
+            var result = await _controller.Body() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+        
+        [Test]
+        public void WhenBreadCrumbCalled_ReturnHtml()
+        {
+            var result = _controller.Breadcrumb() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task WhenBodyTopCalled_ReturnHtml()
+        {
+            var result = await _controller.BodyTop() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+        [Test]
+        public async Task WhenBodyTopCalled_ReturnHtmlWithUserName()
+        {
+          
+        
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    HttpContext =
+                    {
+                        User = new ClaimsPrincipal(
+                            new ClaimsIdentity(new List<Claim> {new Claim("CustomerId", "test")},
+                                "testType"))
+                    }
+                }
+            };
+            var result = await _controller.BodyTop() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+            var model = result.ViewData.Model as CompositeViewModel;
+            model.Name.Should().NotBe(null);
+        }
+
+        [Test]
+        public void WhenBodyFooterCalled_ReturnHtml()
+        {
+            var result = _controller.BodyFooter() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+
+       
+        [Test]
+        public async Task WhenBodyCalledWithFormDataAndGoalUpdated_ThenRedirectToBody()
+        {
+            var result = await _controller.Body(GetViewModel(), new FormCollection(new Dictionary<string, StringValues>
+            {
+                {"Day", "1"},
+                {"Month", "3"},
+                {"Year", "2030"}
+            })) as RedirectResult;
+
+            result.Url.Should().Contain("UpdateGoalConfirmation");
+        }
+        
+
+        private ChangeGoalDueDateCompositeViewModel GetViewModel()
+        {
+            var changeGoalDueDateCompositeViewModel = new ChangeGoalDueDateCompositeViewModel()
+                {
+                    Goal = new Goal(){GoalId = new Guid().ToString()}
+                    };
+            
+            return changeGoalDueDateCompositeViewModel;
+        }
+    }
+}
+
---
+++ DFC.App.ActionPlans.UnitTests/Controllers/UpdateGoalConfirmationControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DFC.App.ActionPlans.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+namespace DFC.App.ActionPlans.UnitTests.Controllers
+{
+    class UpdateGoalConfirmationControllerTests : BaseControllerTests
+    {
+        private UpdateGoalConfirmationController _controller;
+
+        [SetUp]
+        public void Init()
+        {
+           
+            _controller = new UpdateGoalConfirmationController(_logger, _compositeSettings, _dssReader);
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+           
+        }
+
+        [Test]
+        public async Task WhenBodyTopCalled_ReturnHtml()
+        {
+            var result = await _controller.BodyTop() as ViewResult;
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().BeNull();
+        }
+    }
+}
