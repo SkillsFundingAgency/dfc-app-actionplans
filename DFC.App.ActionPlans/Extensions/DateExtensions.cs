123c01b8e2083b2c5b7993d549db65961a928df2
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-06-29 16:10:31 +0100

    wip

---
+++ DFC.App.ActionPlans/Extensions/DateExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DFC.App.ActionPlans.Extensions
+{
+    public static class DateExtensions
+    {
+        public static string DateOnly(this DateTime date)
+        {
+            
+            return date.ToString("dd MMMM yyyy");
+        }
+    }
+}
