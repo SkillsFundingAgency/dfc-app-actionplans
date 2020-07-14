242b9f2ea6f6855b84f46b39d304fb2732ce2b9b
Gulshan Baryah <gulshan.baryah@contractsonline.org>
2020-07-14 16:37:24 +0100

    wip

---
+++ DFC.App.ActionPlans/Helpers/Utility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DFC.App.ActionPlans.Services.DSS.Enums;
+using DFC.App.ActionPlans.ViewModels;
+using DFC.Personalisation.Common.Extensions;
+
+namespace DFC.App.ActionPlans.Helpers
+{
+    public static class Utility
+    {
+        public static string GetActionHelperText(SignpostedToCategory? signpostedToCategory, string signpostedTo)
+        {
+            var returnValue = "";
+            if (signpostedToCategory != null)
+            {
+                returnValue = signpostedToCategory.GetDisplayName() + " - ";
+            }
+
+            if (Uri.IsWellFormedUriString(signpostedTo, UriKind.RelativeOrAbsolute))
+            {
+                returnValue += $"<govukLink id='helplink' link-href='{signpostedTo}' LinkText='{signpostedTo}'></govukLink>";
+            }
+            else
+            {
+                returnValue += signpostedTo;
+            }
+
+            return returnValue;
+        }
+    }
+}
